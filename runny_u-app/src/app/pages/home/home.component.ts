11b1af0f345c9b1a6e2489d7d8ea9a6a10439f0f
JohnSebastianV <jvillegas873@soyudemedellin.edu.co>
2025-04-29 18:42:33 -0500

    Se añade la busqueda de restaurantes

--- runny_u-app/src/app/pages/home/home.component.ts
+++ runny_u-app/src/app/pages/home/home.component.ts
@@ -1,20 +1,26 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { RestaurantService } from '../../services/restaurant.service';
 import { Restaurant } from '../../shared/interfaces/restaurant.interface';
 import { Promotion } from '../../shared/interfaces/promotions.interface';
 import { PromotionService } from '../../services/promotion.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [NgFor, RouterModule],
+  imports: [NgFor, RouterModule, FormsModule, NgIf],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  restaurant: Restaurant[] | undefined;
-  promotions: Promotion[] | undefined;
+  restaurant: Restaurant[] = [];
+  promotions: Promotion[] = [];
+
+  searchTerm: string = ''; // para el término de búsqueda
+  filteredRestaurants: Restaurant[] = [];
+  showSearchResults: boolean = false; // controla si el modal aparece
+
 
   router = inject(Router);
   restaurantService = inject(RestaurantService);
@@ -23,6 +29,21 @@ export class HomeComponent {
   ngOnInit(): void {
     this.restaurant = this.restaurantService.getRestaurants();
     this.promotions = this.promotionService.getPromotions();
-  }
-}
 
+    this.filteredRestaurants = this.restaurant; // Mostrar todos inicialmente
+  }
+
+  onSearch(event: Event): void {
+    event.preventDefault();
+    const term = this.searchTerm.toLowerCase().trim();
+    this.filteredRestaurants = this.restaurant.filter(r =>
+      r.name.toLowerCase().includes(term)
+    );
+    this.showSearchResults = true; // abre el modal
+  }
+  
+  closeSearchResults(): void {
+    this.showSearchResults = false;
+  }
+  
+}
